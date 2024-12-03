bce2f71d472dade8333de640db29ecb09abd8b37
RaspiKidd <kerry@raspikidd.com>
2024-12-03 13:30:28 +0000

    Alex week 5 code

--- Alex/week3.py
+++ Alex/week3.py
@@ -1,3 +1,8 @@
+from sense_hat import SenseHat
+import time
+sense = SenseHat()
+w = [150, 150, 150]
+b = [0, 0, 255]
 e = [0, 0, 0]
 image = [
     e,e,e,e,e,e,e,e,
@@ -9,6 +14,10 @@ image = [
     e,e,e,e,e,e,e,e,
     e,e,e,e,e,e,e,e,
     ]
+sense.set_pixels(image)
+while True:
+    time.sleep(1)
+    sense.flip_h()
     
     
     
