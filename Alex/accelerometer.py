251a62fff300a086f6c7a6dd930b83863e6a0c67
RaspiKidd <kerry@raspikidd.com>
2024-12-17 13:42:26 +0000

    Alex's week 7 code

--- Alex/accelerometer.py
+++ Alex/accelerometer.py
@@ -1,9 +1,13 @@
 from sense_hat import SenseHat
 sense = SenseHat()
 while True:
-    pitch, roll, yaw = sense.get_orientstion().values()
+    pitch, roll, yaw = sense.get_orientation().values()
     print("pitch=%s, roll=%s, yaw=%s" % (pitch,yaw,roll))
     from sense_hat import SenseHat
     sense = SenseHat()
     while True:
         x, y, z = sense.get_accelerometer_raw().values()
+        x=round(x, 0)
+        y=round(y, 0)
+        z=round(z, 0)
+        print("x=%s, y=%s, z=%s" % (x, y, z)) 
