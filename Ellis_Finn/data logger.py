54a1fd2b902d629798d2829d42597f42852d66a2
RaspiKidd <kerry@raspikidd.com>
2025-02-04 13:10:37 +0000

    Ellis and Finn's data logger program

--- Ellis_Finn/data logger.py
+++ Ellis_Finn/data logger.py
@@ -1,18 +1,20 @@
 from sense_hat import SenseHat
 from datetime import datetime
-2WRITE_FREQUENCY = 100
+#sense = SenseHat()
+FILENAME = ""
+WRITE_FREQUENCY = 100
 def file_setup(filename):
-    header =["temp_h", "temp_p",humidity","pressure","pitch","roll","yaw","mag_x","mag_y","mag_2","accel_x'
+    header =["temp_h", "temp_p","humidity","pressure","pitch","roll","yaw","mag_x","mag_y","mag_z","accel_x"]
     with open (filename,"w") as f:
         f.write(",".join(str(value) for value in header)+ "\n")
-    log_data():
+def log_data():
     output_string = ",".join(str(value) for value in sense_data)
     batch_data.append(output_string)
-    get_sense_data():
+def get_sense_data():
     sense_data=[]
     
     sense_data.append(sense.get_temperature_from_humidity())
-    sense_data.append(sense.get_temerature_from_pressure())
+    sense_data.append(sense.get_temperature_from_pressure())
     sense_data.append(sense.get_humidity())
     sense_data.append(sense.get_pressure())
     yaw,pitch,roll = sense.get_orientation().values()
@@ -26,4 +28,29 @@ def file_setup(filename):
     
     gyro_x,gyro_y,gyro_z = sense.get_gyroscope_raw().values()
     sense_data.extend([gyro_x,gyro_y,gyro_z])
+    
+    sense_data.append(datetime.now())
+    return sense = SenseHat()
+batch_data= []
+
+if FILENAME == "":
+    filename = "SenseLog-"+str(datetime.now())+".csv"
+else:
+    filename = FILENAME+"-"+str(datetime.now())+".csv"
+    
+file_setup(filename)
+
+while True:
+    sense_data = get_sense_data()
+    log_data()
+    
+    if len(batch_data) >= WRITE_FREQUENCY:
+        print ("Writing to file..")
+        with open(filename,"a") as f:
+            for line in batch_data:
+                f.write(line + "\n")
+            batch_data = []
+
+
+
     
