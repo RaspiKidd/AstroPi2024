a242ececab4ec3ae181e3426c1576ac16076acfc
RaspiKidd <kerry@raspikidd.com>
2025-02-04 13:50:06 +0000

    Alex's data logger

--- Alex/Sense_Logger_V1.py
+++ Alex/Sense_Logger_V1.py
@@ -1,42 +1,58 @@
 from sense_hat import SenseHat
 from datetime import datetime
-
+FILENAME = ""
+WRITE_FREQUENCY = 100
+def file_setup(filename):
+    
+    
+    
+    header =["temp_h","temp_p","humidity","pressure","pitch","roll","yaw","mag_x","mag_y","mag_z","accel_x"]
+    with open(filename,"w") as f:
+              f.write(",".join(str(value) for value in header)+ "\n")
+def log_data():
+    output_string = ",".join(str(value) for value in sense_data)
+    batch_data.append(output_string)
 def get_sense_data():
     sense_data=[]
-    
+             
     sense_data.append(sense.get_temperature_from_humidity())
     sense_data.append(sense.get_temperature_from_pressure())
     sense_data.append(sense.get_humidity())
     sense_data.append(sense.get_pressure())
-    o = sense.get_orientation()
-    yaw = o["yaw"]
-    pitch = o["pitch"]
-    roll = o["roll"]
+             
+    yaw,pitch,roll = sense.get_orientation().values()
     sense_data.extend([pitch,roll,yaw])
-    
-    mag = sense.get_compass_raw()
-    mag_x = mag["x"]
-    mag_y = mag["y"]
-    mag_z = mag["z"]
+             
+    mag_x,mag_y,mag_z = sense.get_compass_raw().values()
     sense_data.extend([mag_x,mag_y,mag_z])
-    
-    acc = sense.get_accelerometer_raw()
-    x = acc["x"]
-    y = acc["y"]
-    z = acc["z"]
+             
+    x,y,z = sense.get_accelerometer_raw().values()
     sense_data.extend([x,y,z])
-    
-    gyro = sense.get_gyroscope_raw()
-    gyro_x = gyro["x"]
-    gyro_y = gyro["y"]
-    gyro_z = gyro["z"]
+             
+    gyro_x,gyro_y,gyro_z = sense.get_gyroscope_raw().values()
     sense_data.extend([gyro_x,gyro_y,gyro_z])
-    
+             
     sense_data.append(datetime.now())
+             
     return sense_data
+             
+sense = SenseHat ()
+batch_data= []
 
-sense = SenseHat()
+if FILENAME == "":
+    filename = "SenseLog-"+str(datetime.now())+".csv"
+else:
+    filename = FILENAME+"-"+str(datetime.now())+".csv"
+
+file_setup(filename)
 
 while True:
     sense_data = get_sense_data()
-    print(sense_data)
+    log_data()
+    
+    if len(batch_data) >= WRITE_FREQUENCY:
+             print("Writing to file..")
+             with open(filename,"a") as f:
+                 for line in batch_data:
+                     f.write(line + "\n")
+                 batch_data = []
