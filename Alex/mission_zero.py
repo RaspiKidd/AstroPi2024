c436a472c3a047fdf84e2779fa5853f9745791c6
RaspiKidd <kerry@raspikidd.com>
2025-02-18 12:46:33 +0000

    Alex's Mission Zero Project

--- Alex/mission_zero.py
+++ Alex/mission_zero.py
@@ -5,23 +5,25 @@ sense.set_rotation(270)
 sense.colour.gain = 60
 sense.colour.integration_cycles = 64
 
-c = (25, 25, 112)
-a = (255, 255, 255)
+p = (212, 158, 125)
 t = (255, 140, 0)
-r = (0, 0, 0)
+a = (255, 255, 255)
+c = (0, 0, 0)
+q = (255, 255, 0)
+d = (100, 149, 237)
 
 for i in range(28):
     rgb = sense.colour
     c = (rgb.red, rgb.green, rgb.blue)
 
     image = [
-    t, a, t, c, c, t, a, t,
-    t, a, t, c, c, t, a, t,
-    t, t, t, t, t, t, t, t,
-    t, a, r, t, t, r, a, t,
-    t, t, t, t, t, t, t, t,
-    a, a, a, r, r, a, a, a,
-    c, a, a, a, a, a, a, c,
-    c, c, a, a, a, a, c, c]
+    a, a, q, q, q, q, a, a,
+    a, q, q, q, q, q, q, a,
+    q, q, q, p, q, q, q, a,
+    q, q, d, p, p, d, p, a,
+    a, q, p, p, p, p, q, a,
+    a, a, d, d, d, d, a, a,
+    a, p, c, c, c, c, p, a,
+    a, a, t, a, a, t, a, a]
 
     sense.set_pixels(image)
