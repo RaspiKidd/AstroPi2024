70ffb2dcd218fd01294d7e7e2ed8df65663bc481
RaspiKidd <kerry@raspikidd.com>
2025-02-25 17:39:42 +0000

    Alex's Mission Zero Project final

--- Alex/pumpkin_animation.py
+++ Alex/pumpkin_animation.py
@@ -9,8 +9,9 @@ o = (252, 127, 3)
 b = (31, 11, 1)
 g = (16, 156, 6)
 w = (235, 255, 183)
-f1 = (255, 59, 5)
-f2 = (255, 46, 3)
+y = (115, 103, 16)
+f1 = (174, 127, 3)
+f2 = (205, 46, 3)
 
 for i in range(28):
     rgb = sense.colour
@@ -31,12 +32,22 @@ image2= [
     w, w, w, g, g, w, w, w,
     w, w, g, g, o, g, w, w,
     w, o, o, o, o, o, o, w,
+    o, o, y, o, o, y, o, o,
+    o, o, o, o, o, o, o, o,
+    o, o, y, y, y, y, o, o,
+    w, o, o, o, o, o, o, w]
+
+image3= [
+    w, w, w, w, g, w, w, w,
+    w, w, w, g, g, w, w, w,
+    w, w, g, g, o, g, w, w,
+    w, o, o, o, o, o, o, w,
     o, o, f1, o, o, f1, o, o,
     o, o, o, o, o, o, o, o,
     o, o, f1, f1, f1, f1, o, o,
     w, o, o, o, o, o, o, w]
 
-image3= [
+image4= [
     w, w, w, w, g, w, w, w,
     w, w, w, g, g, w, w, w,
     w, w, g, g, o, g, w, w,
@@ -46,13 +57,27 @@ image3= [
     o, o, f2, f2, f2, f2, o, o,
     w, o, o, o, o, o, o, w]
     
-
+print("starting")
+sleep(0.2)
 sense.set_pixels(image1)
+print("image1")
 sleep(1)
 sense.set_pixels(image2)
+print("image2")
 sleep(1)
 sense.set_pixels(image3)
+print("image3")
+sleep(1)
+sense.set_pixels(image4)
+print("image4")
+sleep(1)
+sense.set_pixels(image3)
+print("image3")
 sleep(1)
 sense.set_pixels(image2)
+print("image2")
 sleep(1)
 sense.set_pixels(image1)
+print("image1")
+sleep(1)
+print("finished")
