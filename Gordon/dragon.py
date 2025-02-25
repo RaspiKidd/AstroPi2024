1b93bbb3c06510aa2b2c47ec9485640f88629a05
RaspiKidd <kerry@raspikidd.com>
2025-02-25 14:09:11 +0000

    Gordon's Mission Zero Project pt1

--- Gordon/dragon.py
+++ Gordon/dragon.py
@@ -12,21 +12,20 @@ sense.color.intergration_cycles = 64 # The interval at which the reading will be
 
 # Add colour variables and image
 
-b = (105, 105, 105) # Dimgray
-c = (0, 0, 0) # Black
-d = (100, 149, 237) # CornflowerBlue
-v = (255, 0,0) # Red
-z = (153, 50, 204) # DarkOrchid
+b = (0, 163, 204) # Diagram
+g = (0, 0, 0) # Black
+p = (255, 77, 136) # CornflowerBlue
+e = (255, 255, 255)
 
 image = [
-    c,c,v,v,v,c,c,c,
-    c,z,z,z,z,v,c,c,
-    z,b,z,b,z,c,c,c,
-    z,z,z,z,z,v,c,c,
-    c,c,d,d,d,c,c,z,
-    c,z,d,z,z,z,z,c,
-    c,c,d,d,z,c,c,c,
-    c,c,z,c,z,c,c,c]
+    e,e,e,e,e,e,e,e,
+    e,e,b,b,b,b,e,e,
+    b,b,e,b,b,e,b,b,
+    e,b,b,b,b,b,b,e,
+    e,e,b,g,g,b,e,e,
+    b,e,p,b,b,p,e,b,
+    b,b,b,b,b,b,b,b,
+    e,b,b,b,b,b,b,e]
 
 # Display the image
 sense.set_pixels(image)
