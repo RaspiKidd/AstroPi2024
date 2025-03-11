f28641bfe7635bdf85a41c791cf0301c9c1336df
RaspiKidd <kerry@raspikidd.com>
2025-03-11 13:29:43 +0000

    Alex GUIZero Pt1

---
+++ Alex/wanted_poster.py
@@ -0,0 +1,12 @@
+from guizero import App, Text, Picture
+app = App("Wanted!")
+app.bg = "#FBFBD0"
+wanted_text = Text(app, "WANTED")
+wanted_text.text_size = 50
+wanted_text = Text(app, "REWARD £100B +")
+wanted_text.text_size = 30
+wanted_text = Text(app, "Renegade Raider Skin")
+wanted_text.text_size = 30
+wanted_text.font = "Times New Roman"
+Peely = Picture(app, image="/home/Kerry/Downloads/image67930")
+app.display()
