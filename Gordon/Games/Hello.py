75256a47b17ba3624536054768a867bdebcb4ea5
RaspiKidd <kerry@raspikidd.com>
2025-03-18 12:30:26 +0000

    Gordon PyGame Pt2

--- Gordon/Games/Hello.py
+++ Gordon/Games/Hello.py
@@ -3,12 +3,11 @@ pygame.init()
 window = pygame.display.set_mode((500, 400))
 while True:
     pygame.draw.rect(window, (255, 0, 0),
-                             (100, 100, 50, 50))
+                             (0, 0, 50, 50))
+    
+    pygame.draw.rect(window, (0, 0, 255),                     
+                             (80, 0, 50, 50))
     
     pygame.draw.rect(window, (0, 255, 0),
-                             (150, 100, 50, 50))
-    
-    pygame.draw.rect(window, (0, 0, 255),
-                             (200, 100, 50, 50))
+                             (40, 0, 50, 50))
     pygame.display.update()
-    
