f9f09f05f78f753a7528dbefe9068655adc6bdae
RaspiKidd <kerry@raspikidd.com>
2025-05-13 13:16:17 +0100

    Gordon PyGame Pt5

--- Gordon/Games/ANIMATING SHAPES & PATHS.py
+++ Gordon/Games/ANIMATING SHAPES & PATHS.py
@@ -10,6 +10,37 @@ surface = pygame.display.set_mode((windowWidth, windowHeight))
 pygame.display.set_caption ('Pygame Shapes!')
 
 #chunk01
+
+#chunk02
+    
+greenSquareX = windowWidth / 2
+greenSquareY = windowHeight / 2
+blueSquareX = 0.0  
+blueSquareY = 0.0
+blueSquareVX = 1
+blueSquareVY = 1
+    
 while True:
     surface.fill((0, 0, 0))
-    22
+    pygame.draw.rect(surface, (255, 0, 0), (random.randint(0,windowWidth), random.randint(0, windowHeight), 10, 10))
+    surface.fill((0,0,0))
+    pygame.draw.rect(surface, (0, 255,0),
+        (greenSquareX, greenSquareY, 10,10))
+    greensquareX += 1
+    #greenSquareY += 1
+    pygame.draw.rect(surface, (0, 0, 255),
+       (blueSquareX, blueSquare, 10, 10))
+    surface.fill((0,0,0))
+    pygame.draw.rect(surface, (0, 0, 255),
+        (blueSquareX, blusquareY, 10, 10))
+    blueSquareX += blueSquareVX
+    blueSquareY += blueSquareVY
+    blueSquareVX += 0.1
+    blueSquareVY += 0.1
+    for event in GAME_EVENTS.get():
+        if event.type ==GAME_GLOBALS.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+
+    
