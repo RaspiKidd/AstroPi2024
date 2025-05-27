d48f883432193c9a906cf3d1f4e088f229885990
RaspiKidd <kerry@raspikidd.com>
2025-05-27 13:17:45 +0100

    Gordon's Day 1 of 100 days of Python

---
+++ Gordon/100DaysPython/Day 1.py
@@ -0,0 +1,16 @@
+print("Hello Replit")
+print("and how are you?")
+print("""
+Here's an exact
+     BIT
+      OF
+       TEXT
+""")
+print("""Gordon
+May 13th 2025
+I am signing up for Replit's 100 days of Python challenge! I will
+make sure to spend some time every day coding along, for a minimum of
+10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone where ever I happen to be.  No excuses for not coding from the middle of a field!
+I am feeling (:
+you can follow my progress at replit.com/""")
