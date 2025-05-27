085379e196090da1dfe531fc80f7fdf86301a900
RaspiKidd <kerry@raspikidd.com>
2025-05-27 13:48:13 +0100

    Finn & Eillis Day 1 & 2 of 100 days of Python

---
+++ Ellis_Finn/100DaysPython/Day 1.py
@@ -0,0 +1,10 @@
+print ("hi bob")
+print ("hello how are u")
+
+print (""" Finnis
+May 13th 100 BC
+We are signimg up for Replit's 100 of Python challenge!
+We will make sure to spend time every Tuesday coding along, for an hour.
+I'll be using Thonny , an amazing coding programme so i can do this at the library.
+
+I am feeling ;)""")
