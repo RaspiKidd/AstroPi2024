1969f35bbaacb3e3ba63f0502bb8186eed1cbe9f
RaspiKidd <kerry@raspikidd.com>
2025-06-17 12:09:07 +0100

    Finn & Eillis Day 5 of 100 days of Python

---
+++ Ellis_Finn/100DaysPython/day 5.py
@@ -0,0 +1,54 @@
+myName = input("What's your name?: ")
+if myName == "Ellios-Finjimglindaelphabalimbob" :
+ print("Welcome Dudes!")    
+ print("You're just the coolest and best dudes ever seen in the whole galaxy!")
+else:
+ print("who on saturn are you?!")
+ 
+                                                                                                        
+                                                                                                        
+                                                                                                                         
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        hi jfhuhjkgtguytfdserwujhjtoreiuhtoiu5thryiuturhjtirhtjriuetyhw784jh9m6fpe0ouø→|ßðop\7ikewcikominmklo6  xdzxs 
